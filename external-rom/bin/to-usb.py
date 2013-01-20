44f354f3ca5952efe0e2b672e761448dc1fb4303
Fen Qin <fqrouter@gmail.com>
2013-01-21 01:06:52 +0800

    fat32 usb boot working for 720n

--- external-rom/bin/to-usb.py
+++ external-rom/bin/to-usb.py
@@ -1,11 +1,39 @@
 import subprocess
-import sys
+import time
 
 ARCHITECTURE = 'ar71xx'
-ROOTFS_PATH = 'bin/{}/openwrt-{}-generic-rootfs.tar.gz'.format(ARCHITECTURE, ARCHITECTURE)
-KERNEL_PATH = 'bin/{}/openwrt-{}-generic-vmlinux-initramfs.elf'.format(ARCHITECTURE, ARCHITECTURE)
+VERSION = '1.0-snapshot-{}'.format(time.strftime("%Y%m%d%H%M%S", time.localtime()))
+ROOTFS_TAR_GZ = 'bin/{}/openwrt-{}-generic-rootfs.tar.gz'.format(ARCHITECTURE, ARCHITECTURE)
+ROOTFS_PATH = 'bin/{}/{}.rootfs'.format(ARCHITECTURE, VERSION)
+INITRAMFS_PATH = 'bin/{}/openwrt-{}-generic-vmlinux-initramfs.elf'.format(ARCHITECTURE, ARCHITECTURE)
 
-usb_path = '/media/{}'.format(sys.argv[1])
-subprocess.call('rm -rf {}/*'.format(usb_path), shell=True)
-subprocess.call('tar -C {}/ -zxf {}'.format(usb_path, ROOTFS_PATH), shell=True)
-subprocess.call('cp {} {}/linux.elf'.format(KERNEL_PATH, usb_path), shell=True)
+def execute(command):
+    print(command)
+    subprocess.call(command, shell=True)
+
+def prepare_rootfs():
+    execute('dd if=/dev/zero of={} bs=1M count=128'.format(ROOTFS_PATH))
+    execute('losetup /dev/loop6 {}'.format(ROOTFS_PATH))
+    try:
+        execute('mkfs -t ext4 -v /dev/loop6')
+        execute('mkdir bin/{}/rootfs'.format(ARCHITECTURE))
+        execute('mount -t ext4 /dev/loop6 bin/{}/rootfs'.format(ARCHITECTURE))
+        try:
+            execute('tar -xzf {} -C bin/{}/rootfs'.format(ROOTFS_TAR_GZ, ARCHITECTURE))
+            time.sleep(1)
+        finally:
+            execute('umount -l bin/{}/rootfs'.format(ARCHITECTURE))
+    finally:
+        execute('losetup -d /dev/loop6')
+
+def to_usb():
+    execute('rm -rf /opt/usb/*')
+    execute('mkdir /opt/usb/fqrouter')
+    with open('/opt/usb/fqrouter/boot', 'w') as f:
+        f.write('FQROUTER_VERSION={}\n'.format(VERSION))
+        f.write('KERNEL_COMMAND_LINE="board=TL-WR720N console=ttyATH0,115200"\n')
+    execute('cp {} /opt/usb/fqrouter/{}.rootfs'.format(ROOTFS_PATH, VERSION))
+    execute('cp {} /opt/usb/fqrouter/{}.initramfs'.format(INITRAMFS_PATH, VERSION))
+
+prepare_rootfs()
+to_usb()
---
+++ internal-rom/bin/update.py
@@ -0,0 +1,14 @@
+import subprocess
+import os.path
+import sys
+
+if len(sys.argv) < 2:
+    raise Exception('must specify hardware')
+hardware = sys.argv[1]
+hardware_dir = os.path.join(os.path.dirname(__file__), '../{}'.format(hardware))
+generic_dir = os.path.join(os.path.dirname(__file__), '../generic')
+
+subprocess.call('cp .config {}'.format(os.path.join(os.path.dirname(__file__), '../{}'.format(hardware))), shell=True)
+subprocess.call('cp -r -p {}/* .'.format(hardware_dir), shell=True)
+subprocess.call('cp -r -p {}/* .'.format(generic_dir), shell=True)
+subprocess.call('make -j 5', shell=True)
