7684fdd8c6ccc05829125788a38498964daae579
Fen Qin <fqrouter@gmail.com>
2013-01-27 16:17:18 +0800

    upstream list; better network status

--- external-rom/generic/files/usr/lib/lua/luci/controller/fqrouter/network.lua
+++ external-rom/generic/files/usr/lib/lua/luci/controller/fqrouter/network.lua
@@ -4,6 +4,7 @@ function index()
     entry({ 'fqrouter', 'network', 'status' }, call('network_status'), 'Network Status', 10).dependent = false
     entry({ 'fqrouter', 'network', 'upstream', 'status' }, call('upstream_status'), 'Upstream Status', 10).dependent = false
     entry({ 'fqrouter', 'network', 'internet', 'status' }, call('internet_status'), 'Internet Status', 10).dependent = false
+    entry({ 'fqrouter', 'network', 'upstream', 'list' }, call('upstream_list'), 'Upstream List', 10).dependent = false
     entry({ 'fqrouter', 'network', 'ping.js' }, call('network_ping'), 'Network Ping', 10).dependent = false
     entry({ 'fqrouter', 'network', 'lan_ip', 'update' }, call('update_lan_ip'), 'Update Lan IP', 10).dependent = false
     entry({ 'fqrouter', 'network', 'restart' }, call('restart_network'), 'Restart Network', 10).dependent = false
@@ -13,6 +14,7 @@ function network_status()
     luci.http.prepare_content('text/html')
     require'luci.template'.render('fqrouter/network/status', {
         upstream_status_url = luci.dispatcher.build_url('fqrouter', 'network', 'upstream', 'status'),
+        upstream_list_url = luci.dispatcher.build_url('fqrouter', 'network', 'upstream', 'list'),
         internet_status_url = luci.dispatcher.build_url('fqrouter', 'network', 'internet', 'status')
     })
 end
@@ -22,7 +24,7 @@ function upstream_status()
     local upstream_status = '没有与上级无线连接好'
     for i, network in ipairs(netm:get_networks()) do
         if network:gwaddr() then
-            upstream_status = '已经连接上了上级无线，ip是：' .. network:ipaddr()
+            upstream_status = '已经连上了上级无线'
         end
     end
     luci.http.prepare_content('text/plain')
@@ -30,9 +32,59 @@ function upstream_status()
 end
 
 function internet_status()
-    local f = assert(io.popen('wget http://www.ifconfig.me/all.json -q -O -'))
+    local internet_status = '没有与互联网连接好'
+    local f = assert(io.popen('ping 8.8.8.8 -c 1'))
     local output = assert(f:read('*a'))
-    luci.http.write(output)
+    if output:find('1 packets received') then
+        internet_status = '已经连上了互联网'
+    end
+    luci.http.prepare_content('text/plain')
+    luci.http.write(internet_status)
+end
+
+function upstream_list()
+    local iw = require'luci.sys'.wifi.getiwinfo('radio0')
+    local upstream_list = {}
+    local no_upstream_found = true
+    for k, v in ipairs(iw.scanlist or { }) do
+        upstream_list[v.ssid] = {
+            signal_strength=calculate_signal_strength(v),
+            bssid=v.bssid,
+            encryption=get_encryption(v),
+            is_configured=false
+        }
+        no_upstream_found = false
+    end
+    function set_is_configured_flag(section)
+        if 'sta' == section.mode and upstream_list[section.ssid] ~= nil then
+            upstream_list[section.ssid].is_configured=true
+        end
+    end
+    x = require'uci'.cursor()
+    x:foreach('wireless', 'wifi-iface', set_is_configured_flag)
+    require'luci.template'.render('fqrouter/network/upstream-list', {
+        upstream_list = upstream_list,
+        no_upstream_found=no_upstream_found
+    })
+end
+
+function get_encryption(info)
+    if info.encryption.wpa > 0 then
+        return info.encryption.wpa >= 2 and 'psk2' or 'psk'
+    else
+        return nil
+    end
+end
+
+function calculate_signal_strength(info)
+    local qc = info.quality or 0
+    local qm = info.quality_max or 0
+
+    if info.bssid and qc > 0 and qm > 0 then
+        return math.floor((100 / qm) * qc)
+    else
+        return 0
+    end
 end
 
 function network_ping()
