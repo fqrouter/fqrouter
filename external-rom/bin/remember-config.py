f85d7fcf97d585ea2a68f23402b6dd84d54604ae
Fen Qin <fqrouter@gmail.com>
2013-01-28 16:59:04 +0800

    remove unsed bin scripts

--- external-rom/bin/remember-config.py
+++
@@ -1,4 +0,0 @@
-import subprocess
-import os.path
-
-subprocess.call('cp .config {}/../720n/.config'.format(os.path.dirname(__file__)), shell=True)
--- internal-rom/bin/to-spike.py
+++
@@ -1,3 +0,0 @@
-import subprocess
-
-subprocess.call('cp bin/x86/openwrt-x86-generic-combined-ext4.vmdk /mnt/hgfs/host/openwrt-spike', shell=True)
