0a398191a164a7d23a48a089b5b5ac06b9c4f841
Fen Qin <fqrouter@gmail.com>
2013-04-16 11:34:23 +0800

    show notification to indicate we are in the background

--- android/src/fq/router/MainActivity.java
+++ android/src/fq/router/MainActivity.java
@@ -1,12 +1,16 @@
 package fq.router;
 
 import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.v4.app.NotificationCompat;
 import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
 import android.view.Menu;
@@ -107,11 +111,34 @@ public class MainActivity extends Activity implements StatusUpdater {
                 handler.postDelayed(new Runnable() {
                     @Override
                     public void run() {
+                        clearNotification();
                         finish();
                     }
+
                 }, 0);
             }
         }).start();
+    }
+
+    private void showNotification(String text) {
+        Intent intent = new Intent(this, MainActivity.class);
+        PendingIntent pIntent = PendingIntent.getActivity(this, 0, intent, 0);
+        Notification noti = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.drawable.wall_green)
+                .setContentTitle("fqrouter is running")
+                .setContentText(text)
+                .setContentIntent(pIntent)
+                .build();
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        noti.flags |= Notification.FLAG_ONGOING_EVENT;
+        notificationManager.notify(0, noti);
+    }
+
+    private void clearNotification() {
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        notificationManager.cancel(0);
     }
 
     private void onReportErrorClicked() {
@@ -137,6 +164,7 @@ public class MainActivity extends Activity implements StatusUpdater {
             public void run() {
                 TextView textView = (TextView) findViewById(R.id.statusTextView);
                 textView.setText(status);
+                showNotification(status);
             }
         }, 0);
     }
