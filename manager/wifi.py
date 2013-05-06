7d76ae3aed48b7251fded3e4fa84d8a912a7c6d6
Fen Qin <fqrouter@gmail.com>
2013-05-06 12:47:12 +0800

    do not hard code the channel to 6

--- manager/wifi.py
+++ manager/wifi.py
@@ -518,7 +518,6 @@ def start_p2p_persistent_network(iface, control_socket_dir, ssid, password, sets
 
 def reset_p2p_channels(iface, control_socket_dir, channel, reg_class):
     try:
-        channel = channel or 6
         shell_execute('%s -p %s -i %s set p2p_oper_channel %s' %
                       (P2P_CLI_PATH, control_socket_dir, iface, channel))
         shell_execute('%s -p %s -i %s set p2p_oper_reg_class %s' %
