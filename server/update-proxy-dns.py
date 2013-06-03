cb6d916df5c2311b24784759aacb869ca21355dd
Fen Qin <fqrouter@gmail.com>
2013-06-03 11:39:20 +0800

    update proxy with log

--- server/update-proxy-dns.py
+++ server/update-proxy-dns.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import sys
 import subprocess
+import traceback
 
 proxies = {}
 current_index = 1
@@ -9,10 +10,14 @@ while True:
     line = line.strip()
     if not line:
         break
-    ip, port = line.split(':')
-    domain = 'proxy%s' % current_index
-    value = 'http-connect:%s:%s::' % (ip, port)
-    print('%s => %s' % (domain, value))
-    subprocess.call('cli53 rrcreate fqrouter.com %s TXT %s --ttl 3600 --replace' % (domain, value), shell=True)
-    current_index += 1
+    print('line: %s' % line)
+    try:
+        ip, port = line.split(':')
+        domain = 'proxy%s' % current_index
+        value = 'http-connect:%s:%s::' % (ip, port)
+        print('%s => %s' % (domain, value))
+        subprocess.call('cli53 rrcreate fqrouter.com %s TXT %s --ttl 3600 --replace' % (domain, value), shell=True)
+        current_index += 1
+    except:
+        traceback.print_exc()
 
