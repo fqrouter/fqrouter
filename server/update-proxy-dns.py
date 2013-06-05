b8589b4a613df108bfa0c36182bb4acaf26e3b42
Fen Qin <fqrouter@gmail.com>
2013-06-05 12:13:57 +0800

    set empty slot for proxy

--- server/update-proxy-dns.py
+++ server/update-proxy-dns.py
@@ -12,12 +12,18 @@ while True:
         break
     print('line: %s' % line)
     try:
-        ip, port = line.split(':')
         domain = 'proxy%s' % current_index
+        ip, port = line.split(':')
         value = 'http-connect:%s:%s::' % (ip, port)
-        print('%s => %s' % (domain, value))
-        subprocess.call('cli53 rrcreate fqrouter.com %s TXT %s --ttl 3600 --replace' % (domain, value), shell=True)
+        proxies[domain] = value
         current_index += 1
     except:
         traceback.print_exc()
+for domain, value in proxies.items():
+    print('%s => %s' % (domain, value))
+    subprocess.call('cli53 rrcreate fqrouter.com %s TXT %s --ttl 3600 --replace' % (domain, value), shell=True)
+for i in range(20 - len(proxies)):
+    domain = 'proxy%s' % (20 - i)
+    print('%s => %s' % (domain, ''))
+    subprocess.call('cli53 rrcreate fqrouter.com %s TXT %s --ttl 3600 --replace' % (domain, '""'), shell=True)
 
