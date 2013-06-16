57a66ab52da9147eac29c7bc57302b07cd36e2de
Fen Qin <fqrouter@gmail.com>
2013-06-16 22:26:31 +0800

    log current user

--- android/src/fq/router/life/LaunchService.java
+++ android/src/fq/router/life/LaunchService.java
@@ -165,6 +165,13 @@ public class LaunchService extends IntentService {
             }
             return process;
         } else {
+            try {
+                appendLog(ShellUtils.sudo(
+                        env, Deployer.PYTHON_LAUNCHER +
+                        " -c \"import os; print 'current user: %s' % os.getuid()\"").trim());
+            }  catch (Exception e) {
+                LogUtils.e("log current user failed", e);
+            }
             return ShellUtils.sudoNoWait(env, Deployer.PYTHON_LAUNCHER + " " +
                     Deployer.MANAGER_MAIN_PY.getAbsolutePath() + " > /data/data/fq.router/log/current-python.log 2>&1");
         }
