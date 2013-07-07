186519f004abc7572469bcb23a53e6bd59ce7950
Fen Qin <fqrouter@gmail.com>
2013-07-07 23:13:34 +0800

    disable youtube scrambler

--- linux/fqrouter.py
+++ linux/fqrouter.py
@@ -51,7 +51,7 @@ def setup():
         '--proxy dynamic,n=20,dns_record=proxy#n#.fqrouter.com,is_public=True,priority=4 '
         '--proxy dynamic,n=5,dns_record=proxy2#n#.fqrouter.com,priority=2 '
         '--proxy dynamic,n=10,type=goagent,dns_record=goagent#n#.fqrouter.com,priority=1 '
-        '--enable-youtube-scrambler '
+        # '--enable-youtube-scrambler '
         '--google-host goagent-google-ip.fqrouter.com '
         '--disable-access-check',
         shell=True))
