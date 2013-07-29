54c3c1820861b4231ba65b29f6c656a15811129a
Fen Qin <fqrouter@gmail.com>
2013-07-29 13:50:37 +0800

    detect if iptables have left over before launch

--- android/src/fq/router2/life_cycle/ManagerProcess.java
+++ android/src/fq/router2/life_cycle/ManagerProcess.java
@@ -1,7 +1,10 @@
 package fq.router2.life_cycle;
 
+import fq.router2.utils.IOUtils;
 import fq.router2.utils.LogUtils;
 import fq.router2.utils.ShellUtils;
+
+import java.io.File;
 
 public class ManagerProcess {
 
@@ -47,6 +50,10 @@ public class ManagerProcess {
     }
 
     public static String getRunMode() throws Exception {
+        File runModeCacheFile = new File("/data/data/fq.router2/etc/run-mode");
+        if (runModeCacheFile.exists()) {
+            return IOUtils.readFromFile(runModeCacheFile);
+        }
         // S4 will fail this test
         try {
             String output = ShellUtils.sudo(ShellUtils.pythonEnv(), Deployer.PYTHON_LAUNCHER +
@@ -54,12 +61,15 @@ public class ManagerProcess {
                     ShellUtils.BUSYBOX_FILE.getCanonicalPath() + "', 'echo', 'hello']))\"").trim();
             LogUtils.i("get run mode: " + output);
             if ("hello".equals(output)) {
+                IOUtils.writeToFile(runModeCacheFile, "run-normally");
                 return "run-normally";
             } else {
+                IOUtils.writeToFile(runModeCacheFile, "run-needs-su");
                 return "run-needs-su";
             }
         } catch (Exception e) {
             LogUtils.e("failed to test subprocess", e);
+            IOUtils.writeToFile(runModeCacheFile, "run-needs-su");
             return "run-needs-su";
         }
     }
