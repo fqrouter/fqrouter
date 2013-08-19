1107aba2fed2eb6be1042484c4e2a967bee4398a
Fen Qin <fqrouter@gmail.com>
2013-08-19 23:18:14 +0800

    update

--- updater/refresh-ss-list.py
+++ updater/refresh-ss-list.py
@@ -3,15 +3,16 @@ import subprocess
 import time
 import random
 import datetime
+import socket
 
 while True:
-    subprocess.call('cli53 rrcreate fqrouter.com ss5 TXT 192.241.219.92:292%s:111222:table --ttl 900 --replace'
-                    % random.randint(0, 9), shell=True)
-    subprocess.call('cli53 rrcreate fqrouter.com ss6 TXT 192.241.214.176:292%s:111222:table --ttl 900 --replace'
-                    % random.randint(0, 9), shell=True)
-    subprocess.call('cli53 rrcreate fqrouter.com ss7 TXT 192.241.216.203:292%s:111222:table --ttl 900 --replace'
-                    % random.randint(0, 9), shell=True)
-    subprocess.call('cli53 rrcreate fqrouter.com ss8 TXT 192.241.225.189:292%s:111222:table --ttl 900 --replace'
-                    % random.randint(0, 9), shell=True)
+    subprocess.call('cli53 rrcreate fqrouter.com ss5 TXT %s:292%s:111222:table --ttl 900 --replace'
+                    % (socket.gethostbyname('socks2.masaila.info'), random.randint(0, 9)), shell=True)
+    subprocess.call('cli53 rrcreate fqrouter.com ss5 TXT %s:292%s:111222:table --ttl 900 --replace'
+                    % (socket.gethostbyname('socks3.masaila.info'), random.randint(0, 9)), shell=True)
+    subprocess.call('cli53 rrcreate fqrouter.com ss5 TXT %s:292%s:111222:table --ttl 900 --replace'
+                    % (socket.gethostbyname('socks4.masaila.info'), random.randint(0, 9)), shell=True)
+    subprocess.call('cli53 rrcreate fqrouter.com ss5 TXT %s:292%s:111222:table --ttl 900 --replace'
+                    % (socket.gethostbyname('socks5.masaila.info'), random.randint(0, 9)), shell=True)
     print('%s done' % datetime.datetime.now())
     time.sleep(900)
