41e7d7acc9189448bd73fbda44144f04659f2b79
Fen Qin <fqrouter@gmail.com>
2013-09-20 16:32:26 +0800

    update ss

--- updater/refresh-ss-list.py
+++ updater/refresh-ss-list.py
@@ -41,19 +41,23 @@ while True:
     try:
         proxies = [
             ('192.81.133.165', 12121, 'wendangku.org', 'aes-256-cfb'),
+            ('198.199.92.59', random.choice([21, 20, 23]), 'u1rRWTssNv0p', 'bf-cfb'),
             ('88.191.139.226', random.choice([21, 20, 23]), 'u1rRWTssNv0p', 'rc4'),
             # ('192.184.80.11', 8318, 'fqrouter', 'rc4'),
+            ('69.163.40.146', 8318, 'fqrouter', 'rc4'),
             ('176.56.236.63', 8318, 'fqrouter', 'rc4'),
             ('162.220.11.191', 8318, 'fqrouter', 'aes-256-cfb'),
             ('192.249.61.233', 8318, 'fqrouter', 'rc4'),
-            ('198.199.92.59', random.choice([21, 20, 23]), 'u1rRWTssNv0p', 'rc4'),
         ]
-        for i, (ip, port, password, encrypt_method) in enumerate(proxies):
-            if check_proxy(ip, port, password, encrypt_method) and check_proxy(ip, port, password, encrypt_method):
+        i = 1
+        for ip, port, password, encrypt_method in proxies:
+            if check_proxy(ip, port, password, encrypt_method) or check_proxy(ip, port, password, encrypt_method):
                 subprocess.call('cli53 rrcreate fqrouter.com ss%s TXT %s:%s:%s:%s --ttl 300 --replace'
-                                % (i + 1, ip, port, password, encrypt_method), shell=True)
-            else:
-                subprocess.call('cli53 rrcreate fqrouter.com ss%s TXT "" --ttl 300 --replace' % (i + 1), shell=True)
+                                % (i, ip, port, password, encrypt_method), shell=True)
+                i += 1
+        for j in range(i, 11):
+            LOGGER.info('[N/A] ss%s.fqrouter.com' % j)
+            subprocess.call('cli53 rrcreate fqrouter.com ss%s TXT "" --ttl 300 --replace' % j, shell=True)
         print('%s done' % datetime.datetime.now())
     except:
         LOGGER.exception('failed to update proxies')
