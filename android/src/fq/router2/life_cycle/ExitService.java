87e3f8094c4f2defc9e64c55693bdbf2a24bc716
Fen Qin <fqrouter@gmail.com>
2013-11-03 20:06:56 +0800

    chmod in new thread

--- android/src/fq/router2/life_cycle/ExitService.java
+++ android/src/fq/router2/life_cycle/ExitService.java
@@ -27,38 +27,46 @@ public class ExitService extends IntentService {
     }
 
     private void exit() {
-        MainActivity.isReady = false;
-        MainActivity.displayNotification(this, getResources().getString(R.string.status_exiting));
-        sendBroadcast(new ExitingIntent());
-        long elapsedTime = StartedAtFlag.delete();
-        if (elapsedTime > 0) {
-            GoogleAnalytics gaInstance = GoogleAnalytics.getInstance(this);
-            Tracker gaTracker = gaInstance.getTracker("UA-37740383-2");
-            gaTracker.setCustomDimension(1, Build.MODEL);
-            gaTracker.setCustomDimension(2, String.valueOf(ShellUtils.isRooted()));
-            gaTracker.sendTiming("engagement", elapsedTime, "session", "session");
-        }
-        LogUtils.i("Exiting, session life " + elapsedTime + "..." );
-        if (ShellUtils.isRooted()) {
-            try {
-                for (File file : new File[]{IOUtils.ETC_DIR, IOUtils.LOG_DIR, IOUtils.VAR_DIR}) {
-                    if (file.listFiles().length > 0) {
-                        ShellUtils.sudo(ShellUtils.BUSYBOX_FILE + " chmod 666 " + file + "/*");
+        try  {
+            MainActivity.displayNotification(this, getResources().getString(R.string.status_exiting));
+            sendBroadcast(new ExitingIntent());
+            long elapsedTime = StartedAtFlag.delete();
+            if (elapsedTime > 0) {
+                GoogleAnalytics gaInstance = GoogleAnalytics.getInstance(this);
+                Tracker gaTracker = gaInstance.getTracker("UA-37740383-2");
+                gaTracker.setCustomDimension(1, Build.MODEL);
+                gaTracker.setCustomDimension(2, String.valueOf(ShellUtils.isRooted()));
+                gaTracker.sendTiming("engagement", elapsedTime, "session", "session");
+            }
+            LogUtils.i("Exiting, session life " + elapsedTime + "..." );
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    if (ShellUtils.isRooted()) {
+                        try {
+                            for (File file : new File[]{IOUtils.ETC_DIR, IOUtils.LOG_DIR, IOUtils.VAR_DIR}) {
+                                if (file.listFiles().length > 0) {
+                                    ShellUtils.sudo(ShellUtils.BUSYBOX_FILE + " chmod 666 " + file + "/*");
+                                }
+                            }
+                        } catch (Exception e) {
+                            LogUtils.e("failed to chmod files to non-root", e);
+                        }
                     }
                 }
+            }).start();
+            stopService(new Intent(this, DownloadService.class));
+            stopService(new Intent(this, AcquireWifiLockService.class));
+            try {
+                ManagerProcess.kill();
             } catch (Exception e) {
-                LogUtils.e("failed to chmod files to non-root", e);
+                LogUtils.e("failed to kill manager process", e);
             }
+            sendBroadcast(new ExitedIntent());
+            MainActivity.clearNotification(this);
+        } finally {
+            MainActivity.isReady = false;
         }
-        stopService(new Intent(this, DownloadService.class));
-        try {
-            ManagerProcess.kill();
-        } catch (Exception e) {
-            LogUtils.e("failed to kill manager process", e);
-        }
-        stopService(new Intent(this, AcquireWifiLockService.class));
-        sendBroadcast(new ExitedIntent());
-        MainActivity.clearNotification(this);
     }
 
     public static void execute(Context context) {
