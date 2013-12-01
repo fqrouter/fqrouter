a7611be71f53983704f78c0cd622e0a5d079ed9e
Fen Qin <fqrouter@gmail.com>
2013-12-02 01:18:09 +0800

    remove ss

--- updater/fabfile.py
+++ updater/fabfile.py
@@ -1,30 +1,6 @@
 from fabric import api
 import os
 
-def all():
-    api.env.hosts = [
-        'root@174.140.169.55',
-        'root@174.140.171.244',
-        'root@199.188.75.60',
-        'root@192.184.80.11',
-        'root@176.56.236.63',
-        'root@162.220.11.191',
-        'root@23.226.131.18',
-        'root@209.141.57.22',
-        'root@174.140.169.62',
-        'root@174.140.169.65',
-        'root@162.217.248.65',
-        'root@162.217.248.91',
-        'root@162.220.240.220',
-        'root@192.249.61.233',
-        'root@198.98.49.121',
-        'root@192.184.94.236',
-        'root@208.117.11.211',
-        'dejavu@192.227.168.26',
-        'dejavu@192.227.161.154',
-        'root@199.175.49.97',
-    ]
-
 def deploy():
     api.put(os.path.join(os.path.dirname(__file__), 'deploy.py'), '~/deploy.py')
     api.sudo('python ~/deploy.py %s' % api.env.host)
